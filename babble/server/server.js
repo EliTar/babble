2e92881ec91bd1255252ff9697eac8a131eea322
EliTar <elitar007@gmail.com>
2017-05-25 12:49:52 +0300

    Learning to write a server

---
+++ babble/server/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log(request.url);
+    response.end();
+});
+
+server.listen(9000);
+
+console.log("hello");
