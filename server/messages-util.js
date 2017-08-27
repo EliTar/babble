5e905237c57b3fe5f26caf214c6baeffdb2eec5a
Eli Tarnarutsky <elitar007@gmail.com>
2017-08-27 03:45:42 +0300

    Fix message deletion bug

--- server/messages-util.js
+++ server/messages-util.js
@@ -4,15 +4,20 @@ var messagesArray = [];
 var messagesID = 0;
 var numberOfDeletedMessages = 0;
 
+function getNumberOfDeletedMessages() {
+    return numberOfDeletedMessages;
+}
+
 function getMessages(counter) {
     var start = counter - numberOfDeletedMessages;
     start = start >= 0 ? start : 0;
-    return messagesArray.slice(start, messagesArray.length);
+
+    return messagesArray.slice(counter, messagesArray.length);
 }
 
 function addMessage(message) {
     message.id = messagesID;
-    message.emailHash = crypto.createHash('md5').update(message.email).digest("hex");
+    message.emailHash = crypto.createHash('md5').update(String(message.email)).digest("hex");
     messagesArray.push(message);
     return messagesID++;
 }
@@ -21,14 +26,15 @@ function deleteMessage(id) {
     messageToDelete = messagesArray.filter(function(message) { return message.id === id; });
     indexOfMessageToDelete = messagesArray.indexOf(messageToDelete[0]);
 
-    messagesArray.splice(indexOfMessageToDelete, 1);
+    messagesArray[indexOfMessageToDelete] = "";
     numberOfDeletedMessages++;
+
+    // messagesArray.splice(indexOfMessageToDelete, 1);
 }
 
 module.exports = {
     messagesArray: messagesArray,
-    messagesID: messagesID,
-    numberOfDeletedMessages: numberOfDeletedMessages,
+    getNumberOfDeletedMessages: getNumberOfDeletedMessages,
     getMessages: getMessages,
     addMessage: addMessage,
     deleteMessage: deleteMessage
